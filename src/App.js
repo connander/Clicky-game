1377d96543217286aaed7d25ece849cc463063cc
connander <connorpstark@gmail.com>
2019-11-17 17:08:05 -0600

    finished logic and initial bug testing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
 import React from "react";
-import FriendCard from "./components/FriendCard";
+import Card from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
 import friends from "./friends.json";
 import "./App.css";
 
+
 class App extends React.Component {
   state = {
-    friends: friends
+    friends,
+    score: 0,
+    highscore: 0
   };
 
-  removeAThing = id => this.setState({ friends: this.state.friends.filter(item => item.id !== id) });
+endGame = () => {
+  if(this.state.score > this.state.highscore) {
+    this.setState({highscore: this.state.score});
+  }
+  this.state.friends.forEach(card => {
+    card.count = 0;
+  });
+  this.setState({score:0});
+  return true;
+}
+
+clickCount = id => {
+  this.state.friends.find((i,j) => {
+    if(i.id === id) {
+      if(friends[j].count === 0){
+        friends[j].count = friends[j].count + 1;
+        this.setState({score: this.state.score + 1});
+        this.state.friends.sort(() => Math.random() - 0.5)
+        return true;
+      } else {
+        this.endGame();
+      }
+    }
+  });
+}
 
   render() {
     return (
       <Wrapper>
-        <h1 className="title">Friends List</h1>
-        {this.state.friends.map(item =>
-          <FriendCard
-            removeItem={this.removeAThing}
-            name={item.name}
-            image={item.image}
-            occupation={item.occupation}
-            location={item.location}
-          />)}
-      </Wrapper>);
+        <Header score ={this.state.score} highscore = {this.state.highscore}>Clicky Game</Header>
+        {this.state.friends.map(card => (
+          <Card
+          clickCount={this.clickCount}
+          id={card.id}
+          key={card.id}
+          image={card.image} />
+        ))}
+      </Wrapper>
+    );
   }
 }
 
--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -1,11 +1,1 @@
-import React from "react";
-import "./style.css";
-
-const FriendCard = props => (
-  <div className="card" onClick={() => props.clickCount(props.id)}>
-    <div className ="img-container">
-      <img alt={props.name} src={props.image} />
-    </div>
-  </div>
-);
-export default FriendCard;
+export { default } from "./FriendCard";
