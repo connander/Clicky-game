ed16a6231d5fe493c2ffd9c88c5cfa3b38ba90a4
connander <connorpstark@gmail.com>
2019-11-22 13:24:18 -0600

    Updates

---
+++ precache-manifest.972027a4f86fe48fcda66a802ddf6a82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea3e00dc3ad745587e1e801d40c62b79",
+    "url": "/Clicky-game/index.html"
+  },
+  {
+    "revision": "8332821920f550f4a52f",
+    "url": "/Clicky-game/static/css/main.6a16e35a.chunk.css"
+  },
+  {
+    "revision": "80eaa54f4d19792c44c9",
+    "url": "/Clicky-game/static/js/2.c148dbf4.chunk.js"
+  },
+  {
+    "revision": "8332821920f550f4a52f",
+    "url": "/Clicky-game/static/js/main.d58c67ce.chunk.js"
+  },
+  {
+    "revision": "b94bf70277d7ae33ced8",
+    "url": "/Clicky-game/static/js/runtime-main.6d23abd1.js"
+  }
+]);
