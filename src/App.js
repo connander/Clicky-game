31f42a8cb6f2f791a5af5810d3a86f0db729dd60
connander <connorpstark@gmail.com>
2019-11-21 18:55:41 -0600

    changed out images from the ones from the class activity

--- src/App.js
+++ src/App.js
@@ -13,42 +13,42 @@ class App extends React.Component {
     highscore: 0
   };
 
-endGame = () => {
-  if(this.state.score > this.state.highscore) {
-    this.setState({highscore: this.state.score});
-  }
-  this.state.friends.forEach(card => {
-    card.count = 0;
-  });
-  this.setState({score:0});
-  return true;
-}
-
-clickCount = id => {
-  this.state.friends.find((i,j) => {
-    if(i.id === id) {
-      if(friends[j].count === 0){
-        friends[j].count = friends[j].count + 1;
-        this.setState({score: this.state.score + 1});
-        this.state.friends.sort(() => Math.random() - 0.5)
-        return true;
-      } else {
-        this.endGame();
-      }
+  endGame = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({ highscore: this.state.score });
     }
-  });
-}
+    this.state.friends.forEach(card => {
+      card.count = 0;
+    });
+    this.setState({ score: 0 });
+    return true;
+  }
+
+  clickCount = id => {
+    this.state.friends.find((i, j) => {
+      if (i.id === id) {
+        if (friends[j].count === 0) {
+          friends[j].count = friends[j].count + 1;
+          this.setState({ score: this.state.score + 1 });
+          this.state.friends.sort(() => Math.random() - 0.5)
+          return true;
+        } else {
+          this.endGame();
+        }
+      }
+    });
+  }
 
   render() {
     return (
       <Wrapper>
-        <Header score ={this.state.score} highscore = {this.state.highscore}>Clicky Game</Header>
+        <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
         {this.state.friends.map(card => (
           <Card
-          clickCount={this.clickCount}
-          id={card.id}
-          key={card.id}
-          image={card.image} />
+            clickCount={this.clickCount}
+            id={card.id}
+            key={card.id}
+            image={card.image} />
         ))}
       </Wrapper>
     );
--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 const Card = props => (
   <div className="card" onClick={() => props.clickCount(props.id)}>
-    <div className ="img-container">
+    <div className="img-container">
       <img alt={props.name} src={props.image} />
     </div>
   </div>
